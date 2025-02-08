10d9609d4c39604a96a77e1df49441368e681386
Hardiksingla15 <hardiksingla20@gmail.com>
2025-02-09 02:09:13 +0530

    Solution to Find the First Peak Element

--- Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
+++ Find the First Peak Element@@676c02002dbc8c3e7a280b6e/code.c
@@ -27,9 +27,10 @@ int main() {
     if (peak != -1) {
         printf("%d\n", peak);
     } else {
-        printf("No peak element found\n");
+        printf("-1\n");
     }
 
     return 0;
 }
+
 
