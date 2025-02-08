3ae172b147db8ae1b7caf2ec018424aacb7ef6f6
Hardiksingla15 <hardiksingla20@gmail.com>
2025-02-08 18:56:19 +0530

    Solution to Print Floyd's Triangle

--- Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
+++ Print Floyd's Triangle@@67877e1818d53eb724f435c7/code.c
@@ -4,9 +4,7 @@ void printPyramid(int n) {
     int num = 1; // Starting number
     for (int i = 1; i <= n; i++) {
         // Print leading spaces
-        for (int j = i; j < n; j++) {
-            printf(" ");
-        }
+        
         // Print numbers
         for (int j = 1; j <= i; j++) {
             printf("%d ", num);
